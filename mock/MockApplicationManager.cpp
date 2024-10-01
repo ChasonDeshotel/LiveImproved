88ab74f7d38920f7e3859509e458c61e7eb47b2c
Chason Deshotel <me@chasondeshotel.com>
2024-10-01 16:49:45 -0500

    remove unused mock

--- mock/MockApplicationManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "ApplicationManager.h"
-#include "LogHandler.h"
-
-ApplicationManager::ApplicationManager() 
-    : mockLogHandler(LogHandler::getInstance()) {}
-
-LogHandler* ApplicationManager::getLogHandler() {
-    return &mockLogHandler;
-}
