c52235f299cdd559383c3dd8704c354064e56b64
Chason Deshotel <me@chasondeshotel.com>
2024-10-08 22:52:28 -0500

    feat: Add protected default constructor to IWindow interface

--- src/interface/IWindow.h
+++ src/interface/IWindow.h
@@ -14,6 +14,9 @@ public:
     IWindow(IWindow&&) = delete;
     IWindow& operator=(IWindow&&) = delete;
 
+protected:
+    IWindow() = default;
+
     virtual void open() = 0;
     virtual void close() = 0;
     [[nodiscard]] virtual auto getWindowHandle() const -> void * = 0;
