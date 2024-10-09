ea957374fd87d0c950c23e83256d4d70757c7c9b
Chason Deshotel <me@chasondeshotel.com>
2024-10-08 22:51:10 -0500

    feat: Add copy and move semantics to ILogHandler

--- src/interface/ILogHandler.h
+++ src/interface/ILogHandler.h
@@ -13,16 +13,22 @@ enum class LogLevel {
 
 class ILogHandler {
 public:
-    virtual ~ILogHandler() = default;
+  virtual ~ILogHandler() = default;
 
-    virtual void log(const std::string& message, LogLevel level = LogLevel::LOG_INFO) = 0;
-    virtual void debug(const std::string& message) = 0;
-    virtual void info(const std::string& message) = 0;
-    virtual void warn(const std::string& message) = 0;
-    virtual void error(const std::string& message) = 0;
+  ILogHandler(const ILogHandler &) = default;
+  ILogHandler(ILogHandler &&) = delete;
+  ILogHandler &operator=(const ILogHandler &) = default;
+  ILogHandler &operator=(ILogHandler &&) = delete;
 
-    virtual void setLogLevel(LogLevel level) = 0;
+  virtual void log(const std::string &message,
+                   LogLevel level = LogLevel::LOG_INFO) = 0;
+  virtual void debug(const std::string &message) = 0;
+  virtual void info(const std::string &message) = 0;
+  virtual void warn(const std::string &message) = 0;
+  virtual void error(const std::string &message) = 0;
+
+  virtual void setLogLevel(LogLevel level) = 0;
 
 protected:
-    virtual std::string logLevelToString(LogLevel level) = 0;
+    virtual auto logLevelToString(LogLevel level) -> std::string = 0;
 };
