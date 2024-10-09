061cdfc89043a8d33110e75a49af0f166bf09fac
Chason Deshotel <me@chasondeshotel.com>
2024-10-08 22:58:21 -0500

    feat: Add copy and move semantics to ILiveInterface

--- src/interface/ILiveInterface.h
+++ src/interface/ILiveInterface.h
@@ -7,11 +7,16 @@ using WindowHandle = const void*;
 
 class ILiveInterface {
 public:
-    virtual ~ILiveInterface() = default;
+  virtual ~ILiveInterface() = default;
 
-    virtual void closeFocusedPluginWindow() = 0;
-    virtual void closeSpecificWindow(WindowHandle element) = 0;
+  ILiveInterface(const ILiveInterface &) = default;
+  ILiveInterface(ILiveInterface &&) = delete;
+  ILiveInterface &operator=(const ILiveInterface &) = default;
+  ILiveInterface &operator=(ILiveInterface &&) = delete;
 
-    virtual void setupPluginWindowChangeObserver(std::function<void()> callback) = 0;
-    virtual void removePluginWindowChangeObserver() = 0;
+  virtual void closeFocusedPluginWindow() = 0;
+  virtual void closeSpecificWindow(WindowHandle element) = 0;
+
+  virtual void setupPluginWindowChangeObserver(std::function<void()> callback) = 0;
+  virtual void removePluginWindowChangeObserver() = 0;
 };
