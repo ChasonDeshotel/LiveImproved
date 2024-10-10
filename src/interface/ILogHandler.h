113d1ab0d5f51ec034ec9f801f560cee8e002d8f
Chason Deshotel <me@chasondeshotel.com>
2024-10-09 21:46:49 -0500

    tidy

--- src/interface/ILogHandler.h
+++ src/interface/ILogHandler.h
@@ -5,23 +5,23 @@
 
 class ILogHandler {
 public:
-  virtual ~ILogHandler() = default;
+    virtual ~ILogHandler() = default;
 
-  ILogHandler(const ILogHandler &) = default;
-  ILogHandler(ILogHandler &&) = delete;
-  ILogHandler &operator=(const ILogHandler &) = default;
-  ILogHandler &operator=(ILogHandler &&) = delete;
+    ILogHandler(const ILogHandler &) = default;
+    ILogHandler(ILogHandler &&) = delete;
+    ILogHandler &operator=(const ILogHandler &) = default;
+    ILogHandler &operator=(ILogHandler &&) = delete;
 
-  virtual void log(const std::string &message,
+    virtual void log(const std::string &message,
                    LogLevel level = LogLevel::LOG_INFO) = 0;
-  virtual void debug(const std::string &message) = 0;
-  virtual void info(const std::string &message) = 0;
-  virtual void warn(const std::string &message) = 0;
-  virtual void error(const std::string &message) = 0;
+    virtual void debug(const std::string &message) = 0;
+    virtual void info(const std::string &message) = 0;
+    virtual void warn(const std::string &message) = 0;
+    virtual void error(const std::string &message) = 0;
 
-  virtual void setLogLevel(LogLevel level) = 0;
+    virtual void setLogLevel(LogLevel level) = 0;
 
 protected:
-  ILogHandler() = default;  // Protected default constructor
-  virtual auto logLevelToString(LogLevel level) -> std::string = 0;
+    ILogHandler() = default;  // Protected default constructor
+    virtual auto logLevelToString(LogLevel level) -> std::string = 0;
 };
