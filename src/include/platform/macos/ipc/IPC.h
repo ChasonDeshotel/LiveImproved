d2f2058bb5577aced17bb03a280de69cda8f58c1
Chason Deshotel <me@chasondeshotel.com>
2024-10-20 19:36:59 -0500

    fix: Refactor IPC class to use static methods

--- src/include/platform/macos/ipc/IPC.h
+++ src/include/platform/macos/ipc/IPC.h
@@ -1,26 +1,30 @@
 #pragma once
 #include <filesystem>
 
-#include "IPCBase.h"
+#include "Types.h"
 
 using Path = std::filesystem::path;
 namespace fs = std::filesystem;
 
-class IPC : public IPCBase {
+class IPC  {
 public:
-    IPC();
-    ~IPC() override;
+    IPC()  = default;
+    ~IPC() = default;
 
     IPC(const IPC &) = delete;
     IPC(IPC &&) = delete;
     IPC &operator=(const IPC &) = delete;
     IPC &operator=(IPC &&) = delete;
 
-    auto createReadPipe() -> bool override;
-    auto createWritePipe() -> bool override;
+    static auto createPipe(const Path& pipePath) -> bool;
+    static auto cleanUpPipe(const Path& path, PipeHandle& handle) -> void;
+    static auto openRequestPipe(const Path& path, PipeHandle& handle) -> bool;
+    static auto openResponsePipe(const Path& path, PipeHandle& handle) -> bool;
 
-    auto cleanUpPipe(const Path& path, PipeHandle& handle) -> void override;
+    static auto drainPipe(int fd, size_t bufferSize) -> void;
 
 private:
-    auto createPipe(const Path& pipePath) -> bool;
+    static constexpr mode_t     DEFAULT_DIRECTORY_PERMISSIONS  {0777};
+    static constexpr mode_t     DEFAULT_PIPE_PERMISSIONS       {0666};
+
 };
