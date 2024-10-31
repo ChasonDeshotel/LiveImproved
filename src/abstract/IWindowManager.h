7b9d457ddd785d21910895f62d709b21bd33cf6e
Chason Deshotel <me@chasondeshotel.com>
2024-10-31 03:02:55 -0500

    refactor: Remove default constructor and move it to protected section

--- src/abstract/IWindowManager.h
+++ src/abstract/IWindowManager.h
@@ -8,22 +8,23 @@ class IWindow;
 
 class IWindowManager {
 public:
-  IWindowManager() = default;  // Add default constructor
-  virtual ~IWindowManager() = default;
+    virtual ~IWindowManager() = default;
 
-  IWindowManager(const IWindowManager &) = default;
-  IWindowManager(IWindowManager &&) = delete;
-  IWindowManager &operator=(const IWindowManager &) = default;
-  IWindowManager &operator=(IWindowManager &&) = delete;
+    IWindowManager(const IWindowManager &) = default;
+    IWindowManager(IWindowManager &&) = delete;
+    IWindowManager &operator=(const IWindowManager &) = default;
+    IWindowManager &operator=(IWindowManager &&) = delete;
 
-  virtual void registerWindow(const std::string &windowName,
+    virtual void registerWindow(const std::string &windowName,
                               std::function<void()> callback) = 0;
-  [[nodiscard]] virtual auto getWindowHandle(const std::string &windowName) const -> void * = 0;
-  virtual void openWindow(const std::string &windowName) = 0;
-  virtual void closeWindow(const std::string &windowName) = 0;
-  virtual void toggleWindow(const std::string &windowName) = 0;
-  [[nodiscard]] virtual auto isWindowOpen(const std::string &windowName) const -> bool = 0;
+    [[nodiscard]] virtual auto getWindowHandle(const std::string &windowName) const -> void * = 0;
+    virtual void openWindow(const std::string &windowName) = 0;
+    virtual void closeWindow(const std::string &windowName) = 0;
+    virtual void toggleWindow(const std::string &windowName) = 0;
+    [[nodiscard]] virtual auto isWindowOpen(const std::string &windowName) const -> bool = 0;
 
 protected:
+    IWindowManager() = default;
+
     virtual auto createWindowInstance(const std::string& windowName) -> std::unique_ptr<IWindow> = 0;
 };
